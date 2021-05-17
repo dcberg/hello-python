402e85efd2f35dda973e5483bb79820c0074935a
Daniel Berg <daniel.berg@digital.ai>
2021-05-17 12:28:13 -0400

    adjust text

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Dan from Python!"
+    return "Hello Everyone from Python!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
